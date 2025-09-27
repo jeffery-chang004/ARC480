7d87aeb209008dc73b8fb464e5afd551a573c06c
jeffery-chang004 <gdbm66@gmail.com>
2025-09-27 02:27:14 -0400

    Update heartbeat_lung.ino

--- heartbeat_lung.ino
+++ heartbeat_lung.ino
@@ -1,17 +1,26 @@
-// C++ code
-//
-
 int signal;
 bool read = false;
 bool timed = false;
 int currentMillis;
 int timeDiff;
 int highest;
+
+const int LED = 3;
+const int BUZZER = 6;
+
 const int enA = 9;
 const int in1 = 8;
 const int in2 = 7;
+
+const int enB = 11;
+const int in3 = 12;
+const int in4 = 13;
+
 const int speed = 60;
 int time;
+int beats;
+bool fanBlow = false;
+bool canAdd = true;
 
 void setup()
 {
@@ -19,6 +28,9 @@ void setup()
   Serial.begin(9600);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 
 }
 
@@ -28,33 +40,62 @@ void loop()
   signal = analogRead(A0);
   if (signal == 0) {
     read = true;
-    timeDiff = millis()/1000;
+    //timeDiff = millis()/1000;
   }if (signal > 800 && read == true) {
     read = false;
   }
 
-  //Serial.println("hi");
-  //Serial.println(signal);
-  Serial.println(String(signal));
+  Serial.println("Signal: " + String(signal) + " reading: " + String(read) + " beats: " + String(beats) + " blow: " + String(fanBlow));
   //Serial.println(String(time));
   //Serial.println("Tme Diff: " + String(timeDiff));
   
-  if (read == true && time < 3) {
-    highest = 0;
-    if (signal > highest) {
-      highest = signal;
+  // if (read == true && time < 3) {
+  //   highest = 0;
+  //   if (signal > highest) {
+  //     highest = signal;
+  //   }
+  // }
+
+  if (read) {
+    analogWrite(enA, 255);
+    analogWrite(enB, 255);
+    if (signal > 500) {
+      digitalWrite(LED, HIGH);
+      digitalWrite(BUZZER, HIGH);
+      if (canAdd) {
+        beats++;
+        canAdd = false;
+        
+      }
+    }else if (signal < 500) {
+      digitalWrite(LED, LOW);
+      digitalWrite(BUZZER, LOW);
+      canAdd = true;
+    }
+    if (fanBlow) {
+      digitalWrite(in1, HIGH);
+      digitalWrite(in3, LOW);
+      digitalWrite(in2, LOW);
+      digitalWrite(in4, HIGH);
+    }else if (!fanBlow) {
+      digitalWrite(in1, LOW);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in2, HIGH);
+      digitalWrite(in4, LOW);
     }
   }
-
-  if (read == true && signal > 450) {
-    digitalWrite(3, HIGH);
-    analogWrite(enA, 255);
-
-  }else if (signal < 450) {
-    digitalWrite(3, LOW);
+  
+  if (read == false) {
     analogWrite(enA, 0);
-
+    analogWrite(enB, 0);
+    beats = 0;
+    digitalWrite(LED, LOW);
+    digitalWrite(BUZZER, LOW);
   }
+  if (beats % 5 == 0) {
+    fanBlow = !fanBlow;
+  }
+
   //digitalWrite(3, HIGH);
   delay(10);
 }
